91dbaf0ecfc5255f2785443a93f3162fe7dc2eda
Th-Denis <thibaut.denis@ut-capitole.fr>
2023-01-24 10:28:23 +0100

    Premier peer-programming 3

--- src/main/java/tp04/metier/Portefeuille.java
+++ src/main/java/tp04/metier/Portefeuille.java
@@ -1,4 +1,4 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -13,27 +13,27 @@ import java.util.Map;
  * @author perussel
  */
 public class Portefeuille {
-    
+
     Map<Action, LignePortefeuille> mapLignes;
-    
+
     private class LignePortefeuille {
-        
+
         private Action action;
-        
+
         private int qte;
-        
+
         public int getQte() {
             return qte;
         }
-        
+
         public void setQte(int qte) {
             this.qte = qte;
         }
-        
+
         public Action getAction() {
             return this.action;
         }
-        
+
         public LignePortefeuille(Action action, int qte) {
             this.action = action;
             this.qte = qte;
@@ -43,16 +43,24 @@ public class Portefeuille {
             return Integer.toString(qte);
         }
     }
-    
+
     public Portefeuille() {
         this.mapLignes = new HashMap();
     }
-    
-    public void acheter(Action a, int q) {
-        if (this.mapLignes.containsKey(a) == false) {
-            this.mapLignes.put(a, new LignePortefeuille(a, q));
+
+    public Action acheter(Action a, int q) {
+        if (q > 0 && a != null) { // 
+            if (!this.mapLignes.containsKey(a)) {
+                
+                this.mapLignes.put(a, new LignePortefeuille(a, q));
+                
+                return a;
+            } else {
+                this.mapLignes.get(a).setQte(this.mapLignes.get(a).getQte() + q);
+                return a;
+            }
         } else {
-            this.mapLignes.get(a).setQte(this.mapLignes.get(a).getQte() + q);
+            return null;
         }
     }
 
@@ -63,9 +71,9 @@ public class Portefeuille {
             } else if (this.mapLignes.get(a).getQte() == q) {
                 this.mapLignes.remove(a);
             }
-        }        
+        }
     }
-    
+
     public String toString() {
         return this.mapLignes.toString();
     }
@@ -76,5 +84,21 @@ public class Portefeuille {
             total = total + (lp.getQte() * lp.getAction().valeur(j));
         }
         return total;
+    }
+
+    /**
+     * Returns the qte of action if it exists in the Protefeuille If the actions
+     * does not exists in the Portefeuille, returns 0
+     *
+     * @param action
+     * @return The quantity in the Portefeuille or 0.
+     */
+    public int getQteAction(Action action) {
+        final LignePortefeuille lp = this.mapLignes.get(action);
+        if (lp != null) {
+            return lp.getQte();
+        } else {
+            return 0;
+        }
     }
 }
