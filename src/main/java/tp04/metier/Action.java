73ee2e900fd0dce29ee729025a9be21c0ae3d38e
Camilo <dlt.camilo@gmail.com>
2023-01-27 14:56:06 +0100

    Move this constructor to comply with Java Code Conventions. Camilo Thibaut

--- src/main/java/tp04/metier/Action.java
+++ src/main/java/tp04/metier/Action.java
@@ -23,6 +23,15 @@ import java.util.Objects;
  */
 public abstract class Action {
 
+    /**
+     * Action constructor
+     * 
+     * @param libelle
+     */
+    protected Action(String libelle) {
+        this.libelle = libelle;
+    }
+
     private String libelle;
 
     /**
@@ -32,15 +41,6 @@ public abstract class Action {
      */
     public String getLibelle() {
         return libelle;
-    }
-
-    /**
-     * Action constructor
-     * 
-     * @param libelle
-     */
-    protected Action(String libelle) {
-        this.libelle = libelle;
     }
 
     public abstract Double valeur(Jour j);
